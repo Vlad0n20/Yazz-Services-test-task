b2e47972a347e1023691de4ac629dc1a5ee5c9bd
Vlad0n23 <vlad.onischuk1234@gmail.com>
2023-12-18 21:35:54 +0200

    Add Shop model

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    class ShopTypeChoices(models.TextChoices):
+        SPORT = 'sport', 'Sport'
+        FOOD = 'food', 'Food'
+        ELECTRONICS = 'electronics', 'Electronics'
+
+    name = models.CharField(max_length=255, verbose_name='Назва')
+    description = models.TextField(null=True, blank=True, verbose_name='Опис')
+    shop_type = models.CharField(max_length=20, choices=ShopTypeChoices, verbose_name='Тип')
+    latitude = models.FloatField(verbose_name='Широта')
+    longitude = models.FloatField(verbose_name='Довгота')
+    sales_commission = models.SmallIntegerField(default=0, verbose_name='Комісія з продажів (відсоток)')
