a32b9c485328845dfffe4018d58caadf993e7422
Vlad0n23 <vlad.onischuk1234@gmail.com>
2023-12-19 15:27:56 +0200

    Register Product, Category and ProductPhoto models in admin

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,13 +1,35 @@
 from django.contrib import admin
 
-from apps.product.models import Product
+from apps.product.models import Product, Category, ProductPhoto
 from apps.product.forms import ProductForm
+
+
+class ProductPhotoInline(admin.StackedInline):  # or use admin.TabularInline for a tabular layout
+    model = ProductPhoto
+    extra = 3
+    fields = ['photo']
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     form = ProductForm
+    inlines = [ProductPhotoInline]
     list_display = ['name', 'shop', 'price', 'weight']
     list_display_links = ['name']
     list_filter = ['shop', 'categories']
     search_fields = ['name', 'description', 'keywords']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'for_shop_type']
+    list_display_links = ['name']
+    list_filter = ['for_shop_type']
+    search_fields = ['name']
+
+
+@admin.register(ProductPhoto)
+class ProductPhotoAdmin(admin.ModelAdmin):
+    list_display = ['product', 'photo']
+    list_display_links = ['product']
+    search_fields = ['product']
