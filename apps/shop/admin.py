b03b425f311789eecc4d3f4a5c20b6ce53d46e34
Vlad0n23 <vlad.onischuk1234@gmail.com>
2023-12-19 15:27:06 +0200

    Register Shop model in admin

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.shop.models import Shop
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type', 'latitude', 'longitude', 'sales_commission']
+    list_display_links = ['name']
+    search_fields = ['name']
+    list_filter = ['type']
