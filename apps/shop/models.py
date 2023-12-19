3d976380da157cbaece275a384b38c3c12699875
Vlad0n23 <vlad.onischuk1234@gmail.com>
2023-12-19 15:26:35 +0200

    Add Category model to shop app

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -9,7 +9,27 @@ class Shop(models.Model):
 
     name = models.CharField(max_length=255, verbose_name='Назва')
     description = models.TextField(null=True, blank=True, verbose_name='Опис')
-    type = models.CharField(max_length=20, choices=ShopTypeChoices, verbose_name='Тип')
+    type = models.CharField(max_length=20, choices=ShopTypeChoices.choices, verbose_name='Тип')
     latitude = models.FloatField(verbose_name='Широта')
     longitude = models.FloatField(verbose_name='Довгота')
     sales_commission = models.SmallIntegerField(default=0, verbose_name='Комісія з продажів (відсоток)')
+
+    def get_category_choices(self):
+        return Category.objects.filter(for_shop_type=self.type).values_list('id', 'name')
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Назва категорії')
+    for_shop_type = models.CharField(max_length=20, choices=Shop.ShopTypeChoices.choices,
+                                     verbose_name='Для типу магазину')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категорія'
+        verbose_name_plural = 'Категорії'
+        ordering = ['name']
