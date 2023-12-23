37a35b360c62ac98c258a8f8f6ea35ffa0e24f14
Vlad0n23 <vlad.onischuk1234@gmail.com>
2023-12-23 20:39:36 +0200

    Update models

--- apps/product/models.py
+++ apps/product/models.py
@@ -5,17 +5,17 @@ from apps.product.services import get_product_photo_upload_path
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Назва')
-    description = models.TextField(null=True, blank=True, verbose_name='Опис')
+    name = models.CharField('Назва', max_length=255)
+    description = models.TextField('Опис', blank=True)
     shop = models.ForeignKey(
         Shop,
         on_delete=models.CASCADE,
         verbose_name='Магазин'
     )
     categories = models.ManyToManyField(Category, verbose_name='Категорії')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Ціна (грн)')
-    weight = models.FloatField(null=True, blank=True, verbose_name='Вага')
-    keywords = models.TextField(null=True, blank=True, verbose_name='Ключові слова')
+    price = models.DecimalField('Ціна (грн)', max_digits=10, decimal_places=2)
+    weight = models.FloatField('Вага', null=True, blank=True)
+    keywords = models.TextField('Ключові слова', null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -28,7 +28,7 @@ class Product(models.Model):
 
 class ProductPhoto(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='photos', verbose_name='Продукт')
-    photo = models.ImageField(upload_to=get_product_photo_upload_path, verbose_name='Фото')
+    photo = models.ImageField('Фото', upload_to=get_product_photo_upload_path)
 
     def __str__(self):
         return self.photo.name
--- apps/shop/models.py
+++ apps/shop/models.py
@@ -7,15 +7,15 @@ class Shop(models.Model):
         FOOD = 'food', 'Food'
         ELECTRONICS = 'electronics', 'Electronics'
 
-    name = models.CharField(max_length=255, verbose_name='Назва')
-    description = models.TextField(null=True, blank=True, verbose_name='Опис')
-    type = models.CharField(max_length=20, choices=ShopTypeChoices.choices, verbose_name='Тип')
-    latitude = models.FloatField(verbose_name='Широта')
-    longitude = models.FloatField(verbose_name='Довгота')
-    sales_commission = models.SmallIntegerField(default=0, verbose_name='Комісія з продажів (відсоток)')
+    name = models.CharField('Назва', max_length=255)
+    description = models.TextField(blank=True, verbose_name='Опис')
+    type = models.CharField('Тип', max_length=20, choices=ShopTypeChoices.choices)
+    latitude = models.DecimalField('Широта', max_digits=9, decimal_places=6)
+    longitude = models.DecimalField('Довгота', max_digits=9, decimal_places=6)
+    sales_commission = models.SmallIntegerField('Комісія з продажів (відсоток)', default=0)
 
     def get_category_choices(self):
-        return Category.objects.filter(for_shop_type=self.type).values_list('id', 'name')
+        return Category.objects.filter(for_shop_type=self.type)
 
     def __str__(self):
         return self.name
@@ -27,9 +27,8 @@ class Shop(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Назва категорії')
-    for_shop_type = models.CharField(max_length=20, choices=Shop.ShopTypeChoices.choices,
-                                     verbose_name='Для типу магазину')
+    name = models.CharField('Назва категорії', max_length=255)
+    for_shop_type = models.CharField('Для типу магазину', max_length=20, choices=Shop.ShopTypeChoices.choices)
 
     def __str__(self):
         return self.name
